1c92255b14203f4c375061635e4188b27bf71919
marianoAlvez <alvez_jmariano@hotmail.com>
2022-03-01 12:14:58 -0300

    Cambios en rutas

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,15 +3,14 @@ import { Route, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-    {
-    path: 'login',
-    component: LoginComponent
-  },
   {
-    path: '**',
-    redirectTo: '/auth/login'
+    path: '',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: '**', redirectTo: 'login' }
+    ]
   }
-];
+]
 
 @NgModule({
   declarations: [],
